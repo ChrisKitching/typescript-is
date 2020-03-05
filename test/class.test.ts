53e44f44f38ecf5c19e9ccdc356ff3368bd1805b
Vytautas Mickus <vmickus@gmx.com>
2020-03-05 13:29:22 +0100

    Check classes but ignore Methods test

--- test/class.test.ts
+++ test/class.test.ts
@@ -6,6 +6,17 @@ export class UsefulObject {
     value:number = 0;
 }
 
+export class UsefulObjectWithConProps {
+    constructor(
+        public name:string,
+        public value:number
+    ) {
+    }
+
+    banana(jesus:'saves') {
+        throw Error('Religion is under specified.');
+    }
+}
 const wildObject = {name:'banana', value: 1};
 
 const wilderObject = {name:'Gene Wilder', value: -1, pockets: ['chocolate']};
@@ -14,5 +25,10 @@ describe('Checking class type', () => {
     it('should be able to check wild objects against class definition', () => {
         assertType<UsefulObject>(wildObject);
         assertType<UsefulObject>(wilderObject);
+    });
+
+    it('understands properties defined in constructor and ignores random methods', () => {
+        assertType<UsefulObjectWithConProps>(wildObject);
+        assertType<UsefulObjectWithConProps>(wilderObject);
     });
 });
